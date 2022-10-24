1c27b540a88877a73bce19c6683d9c790328ab60
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-10-25 00:21:14 +0600

    created an api to serve selected category data

--- index.js
+++ index.js
@@ -7,13 +7,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courseCategories = require("./data/course-categories.json");
+const availableCourses = require("./data/available-courses.json");
 
 app.get("/", (req, res) => {
   res.send("Hello from MW Academy!");
 });
 
+// Serve all Course categories
 app.get("/course-categories", (req, res) => {
   res.send(courseCategories);
+});
+
+// Serve specific category data
+app.get("/category/:id", (req, res) => {
+  console.log(req.params.id);
+  const id = req.params.id;
+  const selectedCategoryCourses = availableCourses.filter(
+    (course) => course.categoryId === +id
+  );
+
+  if (id === "all") {
+    res.send(availableCourses);
+  }
+
+  res.send(selectedCategoryCourses);
 });
 
 app.listen(port, () => {
